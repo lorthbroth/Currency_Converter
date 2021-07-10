135eef00adbc6080595c6a5d665ba595d11e0e19
lorthbroth <dkgoete@gmail.com>
2021-07-10 13:44:42 +0200

    File structuring

---
+++ App/constants/colors.js
@@ -0,0 +1,4 @@
+export default {
+    white: '#fff',
+    border: '#E2E2E2'
+}
---
+++ App/screens/Options.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  View,
+  SafeAreaView,
+} from 'react-native';
+
+import colors from '../constants/colors';
