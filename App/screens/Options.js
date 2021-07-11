a7694095a4b606c2ac94a5a3c479fa348c60db51
lorthbroth <dkgoete@gmail.com>
2021-07-11 11:55:23 +0200

    adding components and styling

--- App/screens/Options.js
+++ App/screens/Options.js
@@ -8,3 +8,37 @@ import {
 } from 'react-native';
 
 import colors from '../constants/colors';
+
+const styles = StyleSheet.create({
+  row: {
+    paddingHorizontal: 20,
+    paddingVertical: 16,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    flexDirection: 'row',
+    backgroundColor: colors.white,
+  },
+});
+
+export default () => {
+  return (
+    <SafeAreaView>
+      <TouchableOpacity onPress={() => alert('todo!')} style={styles.row}>
+        <text style={styles.title}>Themes</text>
+      </TouchableOpacity>
+
+      <View style={styles.seperator} />
+
+      <TouchableOpacity onPress={() => alert('todo!')} style={styles.row}>
+        <Text style={styles.title}>Dominic React Native App</Text>
+      </TouchableOpacity>
+
+      <View style={styles.seperator} />
+
+      <TouchableOpacity onPress={() => alert('todo!')} style={styles.row}>
+        <Text style={styles.title}>Dominic React Native App</Text>
+      </TouchableOpacity>
+
+    </SafeAreaView>
+  );
+};
